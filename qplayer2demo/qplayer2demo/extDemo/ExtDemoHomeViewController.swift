eb0d534bd993ac31636d5a8313a90607fdaf8ddd
Mengwangchao <www.903064748@qq.com>
2022-10-18 19:25:19 +0800

    切集demo

--- qplayer2demo/qplayer2demo/extDemo/ExtDemoHomeViewController.swift
+++ qplayer2demo/qplayer2demo/extDemo/ExtDemoHomeViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import qplayer2_ext
 import qplayer2_core
+import AVFAudio
 class myIlogic:ILogicProvider{
     
 }
@@ -50,38 +51,55 @@ class myICommonPlayerEnvironment:ICommonPlayerEnvironment{
     
 }
 class ExtDemoHomeViewController: UIViewController {
-
+    var config : CommonPlayerConfig<Any, myIlogic, CommonPlayableParams, CommonVideoParams>!
+    var mPlayerDataSource : CommonPlayerDataSource<CommonPlayableParams,CommonVideoParams>!
+    var mCommonPlayer : CommonPlayer<Any, myIlogic, CommonPlayableParams, CommonVideoParams>!
+    var flag : Int!
     override func viewDidLoad() {
         super.viewDidLoad()
 //        var s = PlayerControlPanelContainer<myIlogic, CommonPlayableParams, CommonVideoParams>()
 //        s.show()
-        var view = UIView(frame: CGRect(x: 100, y: 100, width: 300, height: 150))
-        view.backgroundColor = UIColor.blue
+        var view = UIView(frame: CGRect(x: 10, y: 100, width: 300, height: 150))
+//        view.backgroundColor = UIColor.blue
         self.view.addSubview(view)
-        var mPlayerDataSource = self.createCommonPlayerDataSource()
-        
+        self.mPlayerDataSource = self.createCommonPlayerDataSource()
+        flag = 1
         do{
             var service = Set<String>()
-            var config = try CommonPlayerConfig<Any, myIlogic, CommonPlayableParams, CommonVideoParams>.CommonPlayerCoreConfig.Builder()
-                .setPlayerDataSource(playerDataSource: mPlayerDataSource)
+            self.config = try CommonPlayerConfig<Any, myIlogic, CommonPlayableParams, CommonVideoParams>.CommonPlayerCoreConfig.Builder()
+                .setPlayerDataSource(playerDataSource: self.mPlayerDataSource)
                 .addEnviroment(name: "sas", enviroment: myICommonPlayerEnvironment(name: "safas", serviceTypes: service))
                 .setRootUIContanier(rootContainer: view)
                 .enableScreenRender()
                 .build()
-            var mCommonPlayer = CommonPlayer(playerConfig: config)
-            var video = mPlayerDataSource.getVideoParamsList()[0]
+            self.mCommonPlayer = CommonPlayer(playerConfig: config)
+            var videoArray = self.mPlayerDataSource.getVideoParamsList()
+            var video = videoArray[0]
             if video != nil{
-                mCommonPlayer.playerVideoSwitcher.switchVideo(id: video.id, params: nil)
+                self.mCommonPlayer.playerVideoSwitcher.switchVideo(id: video.id, params: nil)
             }
+            var time = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(timeAction), userInfo: nil, repeats: true)
         }catch{
             
         }
-        
 
         
         var dic = ["1":1,"2":2,"3":3]
         var arr = [1,2,3,4,5,6]
         
+    }
+    @objc func timeAction(){
+        var videoArray = self.mPlayerDataSource.getVideoParamsList()
+        var video = videoArray[0]
+        var param = self.mPlayerDataSource.getPlayableParams(videoId: video.id, index: 1)
+        if param != nil && flag == 1{
+            self.mCommonPlayer.playerVideoSwitcher.switchPlayableParamsSubject(playableParams: param!)
+            flag+=1
+        }
+        else{
+            video = videoArray[1]
+            self.mCommonPlayer.playerVideoSwitcher.switchVideo(id: video.id, params: nil)
+        }
     }
     func createCommonPlayerDataSource() -> CommonPlayerDataSource<CommonPlayableParams,CommonVideoParams>{
         var longVideo : CommonPlayerDataSource<CommonPlayableParams,CommonVideoParams>
@@ -89,9 +107,22 @@ class ExtDemoHomeViewController: UIViewController {
         var videoParams  = CommonVideoParams(id: 1)
         var builder = QMediaModel(false)
         builder.addStreamElements("", urlType: QPlayerURLType.QURL_TYPE_QAUDIO_AND_VIDEO, url: "http://demo-videos.qnsdk.com/qiniu-1080p.mp4", quality: 1080, isSelected: true, backupUrl: "", referer: "", renderType: QPlayerRenderType.QPLAYER_RENDER_TYPE_PLANE)
-        var longViedArray : Array<CommonPlayableParams> = [CommonPlayableParams(mediaModel: builder, controlPanelType: "sss", displayOrientation: DisplayOrientation.LANDSCAPE, environmentType: "321", startPos: 0)]
+        var builder2 = QMediaModel(false)
+        builder2.addStreamElements("", urlType: QPlayerURLType.QURL_TYPE_QAUDIO_AND_VIDEO, url: "http://demo-videos.qnsdk.com/shortvideo/nike.mp4", quality: 1080, isSelected: true, backupUrl: "", referer: "", renderType: QPlayerRenderType.QPLAYER_RENDER_TYPE_PLANE)
+        var longViedArray = Array<CommonPlayableParams>()
         
+        longViedArray.append(CommonPlayableParams(mediaModel: builder, controlPanelType: "builder", displayOrientation: DisplayOrientation.LANDSCAPE, environmentType: "builder", startPos: 0))
+        longViedArray.append(CommonPlayableParams(mediaModel: builder2, controlPanelType: "builder2", displayOrientation: DisplayOrientation.LANDSCAPE, environmentType: "builder2", startPos: 0))
+        
+        
+        var videoParams2 = CommonVideoParams(id: 2)
+        var videoParams2Playable = QMediaModel(false)
+        videoParams2Playable.addStreamElements("", urlType: QPlayerURLType.QURL_TYPE_QAUDIO_AND_VIDEO, url: "http://demo-videos.qnsdk.com/bbk-bt709.mp4", quality: 1080, isSelected: true, backupUrl: "", referer: "", renderType: QPlayerRenderType.QPLAYER_RENDER_TYPE_PLANE)
+        
+        var videoParams2PlayableArray = Array<CommonPlayableParams>()
+        videoParams2PlayableArray.append(CommonPlayableParams(mediaModel: videoParams2Playable, controlPanelType: "videoParams2Playable", displayOrientation: DisplayOrientation.LANDSCAPE, environmentType: "videoParams2Playable", startPos: 0))
         dataSourceBuilder.addVideo(videoParams: videoParams, playableParamsArray: longViedArray)
+        dataSourceBuilder.addVideo(videoParams: videoParams2, playableParamsArray: videoParams2PlayableArray)
         return dataSourceBuilder.build()
     }
     
@@ -143,14 +174,4 @@ class mf : NSObject , ICommonVideoPlayEventListener{
     
     
 }
-class lis :NSObject, switchListener{
-    func switchVideoIdSubject(videoId: Dictionary<Int64, Dictionary<String, Any>?>) {
-        
-    }
-    
-    func switchPlayableParamsSubject<T3>(playableParams: T3) where T3 : CommonPlayableParams {
-        
-    }
-    
-    
-}
+
