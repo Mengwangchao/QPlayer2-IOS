347af405cc2d55aa979e5b582c0dcbad4f302bb1
Mengwangchao <www.903064748@qq.com>
2022-12-20 18:31:33 +0800

    添加桥接文件

---
+++ qplayer2demo/qplayer2demo/ExtDemo/qplayer2demo-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+#import "QDataHandle.h"
+#import "QNAppDelegate.h"
+#import "QNClassModel.h"
+#import "QNURLListTableViewCell.h"
